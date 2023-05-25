2db6b56b25392fea6b0afc79fb3fa6363ab9f087
Jeferson-Hernandez <jefry_2558@hotmail.com>
2023-05-25 15:52:15 -0500

    mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const toggle = document.getElementById("toggleMenu");
 const menu = document.getElementById("menu");
-
+const arrowImg = (document.getElementById("arrow-img").ondragstart = () => {
+  return false;
+});
 toggle.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
